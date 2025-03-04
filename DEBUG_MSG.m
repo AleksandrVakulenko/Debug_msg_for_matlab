c7e8d1824aad70c32ce645cebe8d43f542298a4b
Aleksandr Vakulenko <sasha705@mail.ru>
2025-03-04 17:49:17 +0300

    add licence and debug_msg description

--- DEBUG_MSG.m
+++ DEBUG_MSG.m
@@ -1,4 +1,24 @@
-
+% Date: 2025.03.04
+% Version: 1.0
+% Author: Aleksandr Vakulenko
+% Licensed after GNU GPL v3
+%
+% ----INFO----:
+% If global "debug_mgs_status" is eual to "enable":
+% - DEBUG_MSG prints a message in Matlab console
+%
+% If global "debug_mgs_status" is not eual to "enable":
+% - no effect
+%
+% Global flag could be changed by call of DEBUG_MSG_ENABLE(status) with 
+% status equal to "enable" or "disable".
+%
+% Debug messages could be colored in black, orange and red.
+% (default color is black).
+% Debug message always starts with "debug_msg: ".
+% Debug messages could be decorated for call in class constructor or
+% destructor. By default messages are plain text (style = "common").
+% ------------
 
 function DEBUG_MSG(msg, color, style)
 arguments
